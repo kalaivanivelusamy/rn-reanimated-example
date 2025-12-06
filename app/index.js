d7b40475852101c9b48ee6574d0f9107855c3a6a
KayVee <kalai.v.vani@gmail.com>
2025-12-06 10:19:05 +0530

    fixed alignment and spacing

--- app/index.js
+++ app/index.js
@@ -1,29 +1,30 @@
 import { useState } from 'react';
-import { Button,View, Text } from 'react-native';
+import { Button,View, Text, Pressable } from 'react-native';
 import Animated, {useSharedValue, withSpring} from 'react-native-reanimated';
 
 export default function Index() {
-  const width = useSharedValue(100);
+const width = useSharedValue(150);
 const [isPressed, setIsPressed] = useState(false);
 
   const handlePress = () => {
     setIsPressed(!isPressed);
-     isPressed ? width.value = withSpring(width.value + 150) : width.value = withSpring(width.value - 150);
+     isPressed ? width.value = withSpring(width.value + 150) : width.value = withSpring(width.value-150);
   }
 
   return (
     <View style={{ flex: 1, alignItems: 'center' }}>
     <Animated.View
       style={{
+        marginTop: 100,
         backgroundColor: "violet",
-        width,
+        width:width,
         height: 100,
       }}
     >
     </Animated.View>
-    <Button title='Click here' onPress={handlePress} style={{ marginTop: 40, padding: 10 }}>
+    <Pressable  onPress={handlePress} style={{ marginTop: 40, padding: 10,backgroundColor: 'lightblue', borderRadius: 5 }}>
       <Text>Click here</Text>
-      </Button>
+      </Pressable>
     </View>
   );
 }
